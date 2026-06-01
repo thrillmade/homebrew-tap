6554df6c353e5a48dbea1b12e62c39ac5e1f7bf3
thrillmot <devintwilmot@gmail.com>
2026-06-01 14:35:53 -0400

    Fix clud-bug.rb: std_npm_args (deprecated std_npm_install_args) (#2)
    Caught by `brew audit --strict --new thrillmade/tap/clud-bug`. Modern
    Homebrew Node-package formulas use `Language::Node.std_npm_args(prefix:
    libexec)` instead of the older `std_npm_install_args(libexec)`.

--- Formula/clud-bug.rb
+++ Formula/clud-bug.rb
@@ -8,7 +8,7 @@ class CludBug < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *Language::Node.std_npm_args(prefix: libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
